64e8f98a56dfb348437421a73bd46bb6cbc6c79d
rjarrin <jarrin.ricardo@gmail.com>
2024-05-03 20:37:17 -0400

    Implemented reverseString() w/ tests

--- src/stringUtils.js
+++ src/stringUtils.js
@@ -1,4 +1,7 @@
-/* eslint-disable import/prefer-default-export */
 export function capitalize(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+export function reverseString(string) {
+    return string.split('').reverse().join('');
 }
--- src/tests/stringUtils.test.js
+++ src/tests/stringUtils.test.js
@@ -1,7 +1,12 @@
 /* eslint-disable no-undef */
-import { capitalize } from '../stringUtils';
+import { capitalize, reverseString } from '../stringUtils';
 
 test('capitalize the first character of the string', () => {
     expect(capitalize('hello')).toBe('Hello');
     expect(capitalize('world')).toBe('World');
+});
+
+test('reverse a string', () => {
+    expect(reverseString('hello')).toBe('olleh');
+    expect(reverseString('world')).toBe('dlrow');
 });
